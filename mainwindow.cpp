6c5df0f7732cd83128df68d2e4eaae83a4bbb94f
朱国华 <zgh_email@163.com>
2019-09-03 20:23:57 +0800

    add ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,17 +7,21 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     // 重新配置窗体大小
-    this->resize(1080,768);
-
+//    this->resize(1366,768);
+    this->resize(1500,845);
 
     /**************************************************/
-    QListWidget *list_function = new QListWidget();
+    list_function = new QListWidget();
     /*车辆控制*/
-    QListWidgetItem *control_item = new QListWidgetItem(QIcon(":/Icon/unactive_car.png"),tr("Control"));
+    QListWidgetItem *control_item = new QListWidgetItem(QIcon(":/Icon/unactive_car.png"),tr("控制"));
     list_function->addItem(control_item);
     /*检测模块*/
-    QListWidgetItem *detect_item = new QListWidgetItem(QIcon(":/Icon/unactive_car.png"),tr("Control"));
+    QListWidgetItem *detect_item = new QListWidgetItem(QIcon(":/Icon/unactive_detect.png"),tr("检测"));
     list_function->addItem(detect_item);
+    /*路径规划模块*/
+    QListWidgetItem *path_item = new QListWidgetItem(QIcon(":/Icon/unactive_path.png"),tr("路径"));
+    list_function->addItem(path_item);
+
     /*整体列表配置*/
     list_function->setViewMode(QListWidget::IconMode);//设置显示模式为图片模式
     list_function->setDragEnabled(false);//控件不允许拖动
@@ -25,32 +29,260 @@ MainWindow::MainWindow(QWidget *parent) :
     list_function->setResizeMode(QListWidget::Adjust);//
     list_function->setMovement(QListWidget::Static);
 
-    list_function->setIconSize(QSize(list_function->width(),100));
+    list_function->setIconSize(QSize(60,60));
     list_function->setMinimumHeight(100);
+    list_function->setFixedWidth(70);
+
+    list_function->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);//关闭水平滚动条
+    list_function->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);//关闭垂直滚动条
+
+//    list_function->setStyleSheet(
+//                "QListWidget{border:1px solid gray; color:black; }"
+//                );
+
     /**************************************************/
-    QStackedWidget *stack_Widget = new QStackedWidget();
 
-    QPushButton *button_G1 = new QPushButton(tr("QGridLayout第一个按钮"));
-    QPushButton *button_G2 = new QPushButton(tr("QGridLayout第二个按钮"));
+    /*** Control UI ***/
+    /* CAN 配置 Group */
+    button_CanConnect = new QPushButton(tr("连接"));
+    button_CanOpen    = new QPushButton(tr("打开"));
+    button_CanClose   = new QPushButton(tr("复位"));
 
+    QVBoxLayout *gCanButtonLayout = new QVBoxLayout();
+    gCanButtonLayout->addWidget(button_CanConnect);
+    gCanButtonLayout->addWidget(button_CanOpen);
+    gCanButtonLayout->addWidget(button_CanClose);
+    gCanButtonLayout->setMargin(3);
 
+    QGroupBox *gCanGroup = new QGroupBox();
+    gCanGroup->setTitle("CAN Configure");
+    gCanGroup->setFixedHeight(120);
+    gCanGroup->setLayout(gCanButtonLayout);
+
+    /* 车辆状态显示 Group */
+    QLabel *label_VCU_Text = new QLabel();
+    label_VCU_Text->setText("VCU:");
+    QLabel *label_VCU_Value = new QLabel();
+    label_VCU_Value->setText("0");
+
+    QLabel *label_ESC_Text = new QLabel();
+    label_ESC_Text->setText("ESC:");
+    QLabel *label_ESC_Value = new QLabel();
+    label_ESC_Value->setText("0");
+
+    QLabel *label_EPS_Text = new QLabel();
+    label_EPS_Text->setText("EPS:");
+    QLabel *label_EPS_Value = new QLabel();
+    label_EPS_Value->setText("0");
+
+    QGridLayout *gVehicleStatusLayout = new QGridLayout();
+    gVehicleStatusLayout->addWidget(label_VCU_Text,0,0);
+    gVehicleStatusLayout->addWidget(label_VCU_Value,0,1);
+    gVehicleStatusLayout->addWidget(label_ESC_Text,1,0);
+    gVehicleStatusLayout->addWidget(label_ESC_Value,1,1);
+    gVehicleStatusLayout->addWidget(label_EPS_Text,2,0);
+    gVehicleStatusLayout->addWidget(label_EPS_Value,2,1);
+    gVehicleStatusLayout->setColumnMinimumWidth(0,30);
+
+    QGroupBox *gVehicleStatusGroup = new QGroupBox();
+    gVehicleStatusGroup->setTitle("Status");
+    gVehicleStatusGroup->setFixedHeight(100);
+    gVehicleStatusGroup->setLayout(gVehicleStatusLayout);
+
+    gVehicleStatusGroup->setStyleSheet("QGroupBox{color:green;\
+                                                  font-weight:20px;\
+                                                  font:bold;\
+                                                 }");
+
+//    gVehicleStatusGroup->setStyleSheet(QString("QGroupBox{subcontrol-origin: margin;subcontrol-position: top center;padding: 0 3px;background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,stop: 0 #FF0ECE, stop: 1 #FFFFFF);}"));
+    /* 车辆转向显示 Group */
+    QLabel *label_Angle_Text = new QLabel();
+    label_Angle_Text->setText("角度:");
+    QLabel *label_Angle_Value = new QLabel();
+    label_Angle_Value->setText("0");
+
+    QLabel *label_AngularVelocity_Text = new QLabel();
+    label_AngularVelocity_Text->setText("角速度:");
+    QLabel *label_AngularVelocity_Value = new QLabel();
+    label_AngularVelocity_Value->setText("0");
+
+    QGridLayout *gVehicleEPSLayout = new QGridLayout();
+    gVehicleEPSLayout->addWidget(label_Angle_Text,0,0);
+    gVehicleEPSLayout->addWidget(label_Angle_Value,0,1);
+    gVehicleEPSLayout->addWidget(label_AngularVelocity_Text,1,0);
+    gVehicleEPSLayout->addWidget(label_AngularVelocity_Value,1,1);
+
+    QGroupBox *gVehicleEPSGroup = new QGroupBox();
+    gVehicleEPSGroup->setTitle("EPS");
+    gVehicleEPSGroup->setFixedHeight(90);
+    gVehicleEPSGroup->setLayout(gVehicleEPSLayout);
+
+    /* 车辆速度显示 Group */
+    QLabel *label_VehicleVelocity_Text = new QLabel();
+    label_VehicleVelocity_Text->setText("车速:");
+    QLabel *label_VehicleVelocity_Value = new QLabel();
+    label_VehicleVelocity_Value->setText("0");
+
+    QLabel *label_RearLeftWheelVelocity_Text = new QLabel();
+    label_RearLeftWheelVelocity_Text->setText("左轮速:");
+    QLabel *label_RearLeftWheelVelocity_Value = new QLabel();
+    label_RearLeftWheelVelocity_Value->setText("0");
+
+    QLabel *label_RearRightWheelVelocity_Text = new QLabel();
+    label_RearRightWheelVelocity_Text->setText("右轮速:");
+    QLabel *label_RearRightWheelVelocity_Value = new QLabel();
+    label_RearRightWheelVelocity_Value->setText("0");
+
+    QLabel *label_RearLeftWheelPulse_Text = new QLabel();
+    label_RearLeftWheelPulse_Text->setText("左脉冲:");
+    QLabel *label_RearLeftWheelPulse_Value = new QLabel();
+    label_RearLeftWheelPulse_Value->setText("0");
+
+    QLabel *label_RearRightWheelPulse_Text = new QLabel();
+    label_RearRightWheelPulse_Text->setText("右脉冲:");
+    QLabel *label_RearRightWheelPulse_Value = new QLabel();
+    label_RearRightWheelPulse_Value->setText("0");
+
+    QGridLayout *gVehicleVelocityLayout = new QGridLayout();
+    gVehicleVelocityLayout->addWidget(label_VehicleVelocity_Text,0,0);
+    gVehicleVelocityLayout->addWidget(label_VehicleVelocity_Value,0,1);
+    gVehicleVelocityLayout->addWidget(label_RearLeftWheelVelocity_Text,1,0);
+    gVehicleVelocityLayout->addWidget(label_RearLeftWheelVelocity_Value,1,1);
+    gVehicleVelocityLayout->addWidget(label_RearRightWheelVelocity_Text,2,0);
+    gVehicleVelocityLayout->addWidget(label_RearRightWheelVelocity_Value,2,1);
+    gVehicleVelocityLayout->addWidget(label_RearLeftWheelPulse_Text,3,0);
+    gVehicleVelocityLayout->addWidget(label_RearLeftWheelPulse_Value,3,1);
+    gVehicleVelocityLayout->addWidget(label_RearRightWheelPulse_Text,4,0);
+    gVehicleVelocityLayout->addWidget(label_RearRightWheelPulse_Value,4,1);
+
+    QGroupBox *gVehicleVelocityGroup = new QGroupBox();
+    gVehicleVelocityGroup->setTitle("Velocity");
+    gVehicleVelocityGroup->setFixedHeight(160);
+    gVehicleVelocityGroup->setLayout(gVehicleVelocityLayout);
+
+    /* vehilce motion group */
+    QLabel *label_Lon_Text = new QLabel();
+    label_Lon_Text->setText("Lon:");
+    QLabel *label_Lon_Value = new QLabel();
+    label_Lon_Value->setText("0");
+
+    QLabel *label_Lat_Text = new QLabel();
+    label_Lat_Text->setText("Lat:");
+    QLabel *label_Lat_Value = new QLabel();
+    label_Lat_Value->setText("0");
+
+    QLabel *label_Yaw_Text = new QLabel();
+    label_Yaw_Text->setText("Yaw:");
+    QLabel *label_Yaw_Value = new QLabel();
+    label_Yaw_Value->setText("0");
+
+    QGridLayout *gVehicleMotionLayout = new QGridLayout();
+    gVehicleMotionLayout->addWidget(label_Lon_Text,0,0);
+    gVehicleMotionLayout->addWidget(label_Lon_Value,0,1);
+    gVehicleMotionLayout->addWidget(label_Lat_Text,1,0);
+    gVehicleMotionLayout->addWidget(label_Lat_Value,1,1);
+    gVehicleMotionLayout->addWidget(label_Yaw_Text,2,0);
+    gVehicleMotionLayout->addWidget(label_Yaw_Value,2,1);
+
+    QGroupBox *gVehicleMotionGroup = new QGroupBox();
+    gVehicleMotionGroup->setTitle("Motion");
+    gVehicleMotionGroup->setFixedHeight(100);
+    gVehicleMotionGroup->setLayout(gVehicleMotionLayout);
+
+    /* Control Input Group */
+    QLabel *label_TargetSteering_Text = new QLabel();
+    label_TargetSteering_Text->setText("方向盘:");
+    QLabel *label_TargetSteering_Value = new QLabel();
+    label_TargetSteering_Value->setText("0");
+
+    QLabel *label_TargetVelocity_Text = new QLabel();
+    label_TargetVelocity_Text->setText("速度:");
+    QLabel *label_TargetVelocity_Value = new QLabel();
+    label_TargetVelocity_Value->setText("0");
+
+    QLabel *label_TargetGear_Text = new QLabel();
+    label_TargetGear_Text->setText("挡位:");
+    QLabel *label_TargetGear_Value = new QLabel();
+    label_TargetGear_Value->setText("0");
+
+    QLabel *label_TargetTorque_Text = new QLabel();
+    label_TargetTorque_Text->setText("扭矩:");
+    QLabel *label_TargetTorque_Value = new QLabel();
+    label_TargetTorque_Value->setText("0");
+
+    QLabel *label_TargetAcc_Text = new QLabel();
+    label_TargetAcc_Text->setText("加速度:");
+    QLabel *label_TargetAcc_Value = new QLabel();
+    label_TargetAcc_Value->setText("0");
+
+    QGridLayout *gVehicleControlInputLayout = new QGridLayout();
+    gVehicleControlInputLayout->addWidget(label_TargetSteering_Text,0,0);
+    gVehicleControlInputLayout->addWidget(label_TargetSteering_Value,0,1);
+    gVehicleControlInputLayout->addWidget(label_TargetVelocity_Text,1,0);
+    gVehicleControlInputLayout->addWidget(label_TargetVelocity_Value,1,1);
+    gVehicleControlInputLayout->addWidget(label_TargetGear_Text,2,0);
+    gVehicleControlInputLayout->addWidget(label_TargetGear_Value,2,1);
+    gVehicleControlInputLayout->addWidget(label_TargetTorque_Text,3,0);
+    gVehicleControlInputLayout->addWidget(label_TargetTorque_Value,3,1);
+    gVehicleControlInputLayout->addWidget(label_TargetAcc_Text,4,0);
+    gVehicleControlInputLayout->addWidget(label_TargetAcc_Value,4,1);
+
+    QGroupBox *gVehicleControlInputGroup = new QGroupBox();
+    gVehicleControlInputGroup->setTitle("Control");
+    gVehicleControlInputGroup->setFixedHeight(160);
+    gVehicleControlInputGroup->setLayout(gVehicleControlInputLayout);
+
+    /*** Control IO Layout ***/
+    QGridLayout *gControl_IO_Layout = new QGridLayout();
+    gControl_IO_Layout->addWidget(gCanGroup,0,0);
+    gControl_IO_Layout->addWidget(gVehicleStatusGroup,1,0);
+    gControl_IO_Layout->addWidget(gVehicleEPSGroup,2,0);
+    gControl_IO_Layout->addWidget(gVehicleVelocityGroup,3,0);
+    gControl_IO_Layout->addWidget(gVehicleMotionGroup,4,0);
+    gControl_IO_Layout->addWidget(gVehicleControlInputGroup,5,0);
+
+    gControl_IO_Layout->setRowStretch(0,1);
+    gControl_IO_Layout->setRowStretch(1,1);
+    gControl_IO_Layout->setRowStretch(2,1);
+    gControl_IO_Layout->setRowStretch(3,1);
+    gControl_IO_Layout->setRowStretch(4,1);
+    gControl_IO_Layout->setRowStretch(5,1);
+    gControl_IO_Layout->setRowStretch(6,1);
+    // Plot 元素
+    mControlPlot = new QCustomPlot();
     QGridLayout *gControlLayout = new QGridLayout();
-    gControlLayout->addWidget(button_G1, 0, 0);        // 第0行0列，第一个参数为控，第二个参数为第几行，第三个参数为第几列
-    gControlLayout->addWidget(button_G2, 1, 1);        // 第1行1列
+    gControlLayout->addLayout(gControl_IO_Layout, 0, 0);
+    gControlLayout->addWidget(mControlPlot, 0, 1);
+    gControlLayout->setColumnStretch(0,1);
+    gControlLayout->setColumnStretch(1,10);
+    gControlLayout->setColumnMinimumWidth(0,100);
 
-    mPlot = new QCustomPlot();
+    /*** Detect UI ***/
+    mDetectPlot = new QCustomPlot();
     QGridLayout *gDetectLayout = new QGridLayout();
-    gDetectLayout->addWidget(mPlot, 0, 1);        // 第2行0列
+    gDetectLayout->addWidget(mDetectPlot, 0, 1);
     gDetectLayout->setColumnStretch(0,1);
     gDetectLayout->setColumnStretch(1,9);
 
+    /*** Path UI ***/
+    mPathPlot = new QCustomPlot();
+    QGridLayout *gPathLayout = new QGridLayout();
+    gPathLayout->addWidget(mPathPlot, 0, 1);
+    gPathLayout->setColumnStretch(0,1);
+    gPathLayout->setColumnStretch(1,9);
+
+
     QWidget *pControlWidget = new QWidget();
     QWidget *pDetectWidget = new QWidget();
+    QWidget *pPathWidget = new QWidget();
     pControlWidget->setLayout(gControlLayout);
     pDetectWidget->setLayout(gDetectLayout);
+    pPathWidget->setLayout(gPathLayout);
 
+    QStackedWidget *stack_Widget = new QStackedWidget();
     stack_Widget->addWidget(pControlWidget);
     stack_Widget->addWidget(pDetectWidget);
+    stack_Widget->addWidget(pPathWidget);
 
     QGridLayout *pMainLayout = new QGridLayout();
     pMainLayout->addWidget(list_function,0,0);
@@ -58,7 +290,7 @@ MainWindow::MainWindow(QWidget *parent) :
     // 设置第0列与第1列的比例为1：9
     pMainLayout->setColumnStretch(0,1);
     pMainLayout->setColumnStretch(1,9);
-    pMainLayout->setColumnMinimumWidth(0,100);
+    pMainLayout->setColumnMinimumWidth(0,70);
 
     QWidget *PMainWidget = new QWidget();
     PMainWidget->setLayout(pMainLayout);
@@ -66,10 +298,20 @@ MainWindow::MainWindow(QWidget *parent) :
     setCentralWidget(PMainWidget);
 
     /****************** single connect slot *********************/
+    /*** List Widget ***/
+    // list 选择变动与 stack widget 界面更新的连接
     connect(list_function,SIGNAL(currentRowChanged(int)),stack_Widget,SLOT(setCurrentIndex(int)));
-
+    // list 的元素变化时相应图片激活状态切换
     connect(list_function,SIGNAL(currentItemChanged(QListWidgetItem*,QListWidgetItem*)),this,SLOT(ProcessItemActiveState(QListWidgetItem*,QListWidgetItem*)));
-//    connect(list_function,SIGNAL(itemActivated(QListWidgetItem*)),this,SLOT(ProcessItemActiveState(QListWidgetItem*)));
+
+    /*** CAN Configure single connect ***/
+    // CAN Connect single and the Slot
+    connect(button_CanConnect,SIGNAL(clicked()),this,SLOT(CAN_Connect()));
+    // CAN Open single and the Slot
+    connect(button_CanOpen,SIGNAL(clicked()),this,SLOT(CAN_Open()));
+    // CAN Close single and the Slot
+    connect(button_CanClose,SIGNAL(clicked()),this,SLOT(CAN_Close()));
+
 }
 
 MainWindow::~MainWindow()
@@ -85,11 +327,83 @@ void MainWindow::ProcessItemActiveState(QListWidgetItem *current, QListWidgetIte
     }
     else
     {
-        current->setIcon(QIcon(":/Icon/active_car.png"));
-        previous->setIcon(QIcon(":/Icon/unactive_car.png"));
+//        qDebug("current Select id:%d;previous select id:%d",this->list_function->row(current),this->list_function->row(previous));
+        switch (this->list_function->row(current)) {
+            case 0:
+                current->setIcon(QIcon(":/Icon/active_car.png"));
+            break;
+
+            case 1:
+                current->setIcon(QIcon(":/Icon/active_detect.png"));
+                break;
+
+            case 2:
+                current->setIcon(QIcon(":/Icon/active_path.png"));
+                break;
+
+            default:
+            break;
+        }
+        switch (this->list_function->row(previous)) {
+            case 0:
+                previous->setIcon(QIcon(":/Icon/unactive_car.png"));
+            break;
+
+            case 1:
+                previous->setIcon(QIcon(":/Icon/unactive_detect.png"));
+                break;
+
+            case 2:
+                previous->setIcon(QIcon(":/Icon/unactive_path.png"));
+                break;
+
+            default:
+            break;
+        }
     }
 }
-void MainWindow::ProcessItemActiveState(QListWidgetItem *current)
+
+void MainWindow::CAN_Connect(void)
 {
-    current->setIcon(QIcon(":/Icon/active_car.png"));
+    if(1 == mWinZlgCan.getConnectStatus())
+    {
+        mWinZlgCan.CanClose();
+        this->button_CanOpen->setEnabled(false);
+        this->button_CanClose->setEnabled(false);
+        this->button_CanConnect->setText("连接");
+    }
+    else
+    {
+        mWinZlgCan.CanConnect();
+        this->button_CanOpen->setEnabled(true);
+        this->button_CanClose->setEnabled(true);
+        this->button_CanConnect->setText("断开");
+    }
+}
+
+void MainWindow::CAN_Open(void)
+{
+    if((0 == mWinZlgCan.CanOpen(0)) && (0 == mWinZlgCan.CanOpen(1)) && (0 == mWinZlgCan.CanOpen(2)))
+    {
+        mWinZlgCan.setOpenStatus(1);
+        this->button_CanConnect->setEnabled(false);
+        this->button_CanClose->setEnabled(true);
+    }
+    else
+    {
+        QMessageBox::information(NULL, "错误", "CAN设备启动失败");
+        mWinZlgCan.setOpenStatus(0);
+        this->button_CanClose->setEnabled(false);
+        this->button_CanConnect->setEnabled(false);
+    }
+}
+
+void MainWindow::CAN_Close(void)
+{
+    mWinZlgCan.CanReset(0);
+    mWinZlgCan.CanReset(1);
+    mWinZlgCan.CanReset(2);
+    mWinZlgCan.setOpenStatus(0);
+    this->button_CanConnect->setEnabled(true);
+    this->button_CanClose->setEnabled(false);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include "QCustomPlot\qcustomplot.h"
+#include "WinZlgCan/win_zlg_can.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -18,12 +20,30 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QCustomPlot *mPlot;
+    // plot variable
+    QCustomPlot *mControlPlot;
+    QCustomPlot *mDetectPlot;
+    QCustomPlot *mPathPlot;
+
     QTimer mDataTimer50ms;
 
+    QListWidget *list_function;
+
+    /* CAN Configure */
+    QPushButton *button_CanConnect;
+    QPushButton *button_CanOpen;
+    QPushButton *button_CanClose;
+    WinZlgCan mWinZlgCan;
+
 private slots:
+    // 功能选择激活函数图片选择的槽函数
     void ProcessItemActiveState(QListWidgetItem *current, QListWidgetItem *previous);
-    void ProcessItemActiveState(QListWidgetItem *current);
+
+    // CAN 配置相关的槽函数
+    void CAN_Connect(void);
+    void CAN_Open(void);
+    void CAN_Close(void);
+
 };
 
 #endif // MAINWINDOW_H
