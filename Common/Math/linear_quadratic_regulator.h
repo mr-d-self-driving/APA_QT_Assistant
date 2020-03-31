2f247bbb2dad03520c22ca518404d1b6e59befef
朱国华 <zgh_email@163.com>
2020-03-31 10:25:28 +0800

    添加运动模型下的LQR控制

--- Common/Math/linear_quadratic_regulator.h
+++ Common/Math/linear_quadratic_regulator.h
@@ -2,11 +2,28 @@
 #define LINEARQUADRATICREGULATOR_H
 
 #include <QMainWindow>
+/**
+ * @brief Eigen矩阵库
+ */
+#include "Eigen/Dense"
 
-class LinearQuadraticRegulator
+namespace math
 {
-public:
-    LinearQuadraticRegulator();
-};
+    /**
+     * @brief Solver for discrete-time linear quadratic problem.
+     * @param A The system dynamic matrix
+     * @param B The control matrix
+     * @param Q The cost matrix for system state
+     * @param R The cost matrix for control output
+     * @param tolerance The numerical tolerance for solving Discrete
+     *        Algebraic Riccati equation (DARE)
+     * @param max_num_iteration The maximum iterations for solving ARE
+     * @param ptr_K The feedback control matrix (pointer)
+     */
+    void SolveLQR(const Eigen::MatrixXd &A, const Eigen::MatrixXd &B,
+                  const Eigen::MatrixXd &Q, const Eigen::MatrixXd &R,
+                  const double tolerance, const uint16_t max_num_iteration,
+                  Eigen::MatrixXd *ptr_k);
+}
 
 #endif // LINEARQUADRATICREGULATOR_H
