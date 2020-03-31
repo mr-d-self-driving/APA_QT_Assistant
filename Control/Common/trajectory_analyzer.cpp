b920e071ae556b96225c525b8a3df68286020cf8
朱国华 <zgh_email@163.com>
2020-03-31 10:25:56 +0800

    目标曲线分析

--- Control/Common/trajectory_analyzer.cpp
+++ Control/Common/trajectory_analyzer.cpp
@@ -2,5 +2,57 @@
 
 TrajectoryAnalyzer::TrajectoryAnalyzer()
 {
+//    track_list = new TrackLinkList();
+}
 
+void TrajectoryAnalyzer::Init(TrackLinkList *list)
+{
+    _trajectory_points_list = list;
+}
+/**
+ * @brief CalculateNearestPoint：计算最近的目标点
+ * @param list：目标曲线数据集
+ * @param a_track：当前车辆跟踪位置
+ * @return 返回离车辆后轴最近的目标曲线点
+ */
+TargetTrack TrajectoryAnalyzer::CalculateNearestPointByPosition(const double x, const double y)const
+{
+    Node<TargetTrack>* track_index_node;
+    Vector2d temp_track;
+    TargetTrack index_node;
+    float min_value;
+
+    temp_track.setX(static_cast<float>(x));
+    temp_track.setY(static_cast<float>(y));
+    index_node.point.setX(0.0);
+    index_node.point.setY(0.0);
+    index_node.yaw = 0.0f;
+    index_node.curvature = 0.0f;
+    if(!_trajectory_points_list)
+    {
+        return index_node;
+    }
+    if(_trajectory_points_list->Length() < 1)
+    {
+        return index_node;
+    }
+
+    track_index_node = _trajectory_points_list->getHeadNode();
+    min_value = (track_index_node->data.point - temp_track).Length();
+    index_node = track_index_node->data;
+    while(track_index_node->next != NULL)
+    {
+        if((track_index_node->data.point - temp_track).Length() < min_value)
+        {
+            min_value = (track_index_node->data.point - temp_track).Length();
+            index_node = track_index_node->data;
+        }
+        track_index_node = track_index_node->next;
+    }
+    return index_node;
+}
+
+double TrajectoryAnalyzer::DistanceToEnd(const double x, const double y)const
+{
+    return static_cast<double>((_trajectory_points_list->getEndNode()->data.point - Vector2d(static_cast<float>(x),static_cast<float>(y))).Length());
 }
--- Control/Common/trajectory_analyzer.h
+++ Control/Common/trajectory_analyzer.h
@@ -1,11 +1,47 @@
 #ifndef TRAJECTORYANALYZER_H
 #define TRAJECTORYANALYZER_H
 
+#include <QMainWindow>
+
+#include "math.h"
+#include "Common/Math/vector_2d.h"
+#include "Common/Utils/Inc/link_list.h"
+#include "Common/VehicleState/GeometricTrack/geometric_track.h"
+#include "Common/Configure/Configs/vehilce_config.h"
+
+typedef struct _TargetTrack
+{
+    Vector2d point;
+    float    yaw;
+    float    curvature;
+    float    velocity;
+}TargetTrack;
+
+/**
+ * @brief The TrackLinkList class：曲线跟踪目标曲线链表
+ */
+class TrackLinkList : public LinkList<TargetTrack>{};
 
 class TrajectoryAnalyzer
 {
 public:
     TrajectoryAnalyzer();
+
+    void Init(TrackLinkList *list);
+
+    /**
+     * @brief CalculateNearestPoint：计算最近的目标点
+     * @param list：目标曲线数据集
+     * @param a_track：当前车辆跟踪位置
+     * @return 返回离车辆后轴最近的目标曲线点
+     */
+    TargetTrack CalculateNearestPointByPosition(const double x, const double y)const;
+
+    double DistanceToEnd(const double x, const double y)const;
+private:
+    Node<TargetTrack>* _track_node;
+    TrackLinkList    * _track_list;
+    TrackLinkList    * _trajectory_points_list;
 };
 
 #endif // TRAJECTORYANALYZER_H
