c29ce29cca53eb4c47618ec0b5c2670cd82093f2
朱国华 <zgh_email@163.com>
2020-04-14 16:51:48 +0800

    目标曲线更新

--- Planning/Curvature/curvature.h
+++ Planning/Curvature/curvature.h
@@ -1,6 +1,7 @@
 #ifndef CURVATURE_H
 #define CURVATURE_H
 
+#include <vector>
 
 #include "math.h"
 #include "Common/Math/vector_2d.h"
@@ -10,8 +11,9 @@
 
 #include "Control/Common/trajectory_analyzer.h"
 
-#define COEFFICIENT_TLS 	( 1.2f  )	// 目标曲线曲率因子
 
+#define SAMPLE_STEP     	( 0.01f )	// 采样步长
+#define COEFFICIENT_TLS 	( 0.1f  )	// 目标曲线曲率因子
 
 class Curvature
 {
@@ -32,7 +34,18 @@ public:
      */
     void GenerateCurvatureSets(TrackLinkList *list,uint16_t type);
 
+    /**
+     * @brief GenerateCurvaturePointSets:使用Vector容器存储目标点信息
+     * @param vec 目标曲线数据集
+     * @param type 曲线类型
+     */
+    void GenerateCurvaturePointSets(std::vector<TargetTrack> *vec,uint16_t type);
 
+    /**
+     * @brief CurvatureCalculate 根据Vector形式的目标曲线数据集，计算曲线的斜率和曲率
+     * @param vec 待计算的数据集
+     */
+    void CurvatureCalculate(std::vector<TargetTrack> *vec);
 
 private:
     TargetTrack _target_track;
