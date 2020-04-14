3100448597dd3c1089dcdf46ab369371c21ed51f
朱国华 <zgh_email@163.com>
2020-04-14 16:50:12 +0800

    使用vector 存储，计算最短距离

--- Control/Common/trajectory_analyzer.h
+++ Control/Common/trajectory_analyzer.h
@@ -2,6 +2,9 @@
 #define TRAJECTORYANALYZER_H
 
 #include <QMainWindow>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
 
 #include "math.h"
 #include "Common/Math/vector_2d.h"
@@ -30,18 +33,38 @@ public:
     void Init(TrackLinkList *list);
 
     /**
-     * @brief CalculateNearestPoint：计算最近的目标点
-     * @param list：目标曲线数据集
-     * @param a_track：当前车辆跟踪位置
-     * @return 返回离车辆后轴最近的目标曲线点
+     * @brief Init 初始化容器
+     * @param vec 待更新的容器
      */
-    TargetTrack CalculateNearestPointByPosition(const double x, const double y)const;
+    void Init(std::vector<TargetTrack> *vec);
 
+    /**
+     * @brief CalculateNearestPointByPosition 基于位置信息计算目标曲线上最近的点
+     * @param x 当前位置x轴坐标
+     * @param y 当前位置y轴坐标
+     * @return 返回最近目标点信息
+     */
+    TargetTrack CalculateNearestPointByPosition(const double x, const double y);
+
+    /**
+     * @brief DistanceToEnd 计算离最后目标曲线点的距离
+     * @param x 当前位置x轴坐标
+     * @param y 当前位置y轴坐标
+     * @return 离最后点的距离值
+     */
     double DistanceToEnd(const double x, const double y)const;
+
+    void TrajectoryTransformToCOM(double rear_to_com_distance);
+
+    Vector2d ComputeCOMPosition(double rear_to_com_distance,TargetTrack path_point);
 private:
     Node<TargetTrack>* _track_node;
     TrackLinkList    * _track_list;
     TrackLinkList    * _trajectory_points_list;
+
+    std::vector<TargetTrack> *_trajectory_points_vector;
+    std::vector<TargetTrack>::iterator _current_it;
+    uint16_t _target_point_index;
 };
 
 #endif // TRAJECTORYANALYZER_H
