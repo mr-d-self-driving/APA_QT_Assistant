aef525ec47a37f6e833b803b2677870756ae0b24
朱国华 <zgh_email@163.com>
2020-05-04 20:23:07 +0800

    添加控制解码

--- Planning/Common/steering_common.h
+++ Planning/Common/steering_common.h
@@ -1,6 +1,17 @@
 #ifndef STEERINGCOMMON_H
 #define STEERINGCOMMON_H
 
+#include <vector>
+#include <cassert>
+#include <limits>
+
+#include "Common/Math/math_utils.h"
+#include "Planning/Common/configuration.h"
+#include "Planning/Path/hc_cc_circle.h"
+
+using namespace std;
+using namespace math;
+
 namespace steering {
 
 /**
@@ -35,6 +46,14 @@ struct State
 };
 
 /**
+ * @brief Checks whether two states are equal
+ * @param state1 :one state
+ * @param state2 :the another state
+ * @return return the result
+ */
+bool StateEqual(const State &state1, const State &state2);
+
+/**
  * @brief The Control struct :Description of a path segment with its corresponding control inputs
  */
 struct Control
@@ -54,6 +73,82 @@ struct Control
      */
     double sigma;
 };
+
+/**
+ * @brief Reverses a control
+ * @param the reversed control
+ */
+void ReverseControl(Control &control);
+
+/**
+ * @brief Subtracts control2 from control1
+ * @param control1
+ * @param control2
+ * @return return the control1 - control2
+ */
+Control SubtractControl(const Control &control1, const Control &control2);
+
+/**
+ * @brief Appends controls with 0 input
+ * @param controls :output the control commond
+ */
+void EmptyControls( vector<Control> &controls );
+
+/**
+ * @brief Appends controls with a straight line
+ * @param q1 :the start configuration point of the straight line
+ * @param q2 :the end configuration point of the straight line
+ * @param controls :output the result to the controls
+ */
+void StraightControls(const Configuration &q1, const Configuration &q2, vector<Control> &controls);
+
+/**
+ * @brief Appends controls with a rs-turn
+ * @param c :the circle of rs-turn
+ * @param q :the configuration q
+ * @param order :the order of control[true-> positive oder, false->negative oder]
+ * @param controls :output the control commond
+ */
+void RS_TurnControls(HC_CC_Circle &c, const Configuration &q, bool order, vector<Control> &controls);
+
+/**
+ * @brief Appends controls with a hc-turn
+ * @param c :the circle of hc-turn
+ * @param q :the configuration q
+ * @param order :the order of control[true-> positive oder, false->negative oder]
+ * @param controls :output the control commond
+ */
+void HC_TurnControls(HC_CC_Circle &c, const Configuration &q, bool order, vector<Control> &controls);
+
+/**
+ * @brief Appends controls with a cc-turn
+ * @param c :the circle of cc-turn
+ * @param q :the configuration q
+ * @param order :the order of control[true-> positive oder, false->negative oder]
+ * @param controls :output the control commond
+ */
+void CC_TurnControls(HC_CC_Circle &c, const Configuration &q, bool order, vector<Control> &controls);
+
+/**
+ * @brief Appends controls with a default cc-turn consisting of two clothoids and a circular arc
+ * @param c :the circle of cc-turn
+ * @param q :the configuration q
+ * @param delta :the angle betwen start configuration of circle and configuration q
+ * @param order :the order of control[true-> positive oder, false->negative oder]
+ * @param controls :output the control commond
+ */
+void CC_DefaultControls(HC_CC_Circle &c, const Configuration &q, double delta, bool order, vector<Control> &controls);
+
+/**
+ * @brief Appends controls with an elementary path if one exists
+ * @param c :the circle of cc-turn
+ * @param q :the configuration q
+ * @param delta :delta :the angle betwen start configuration of circle and configuration q
+ * @param order :the order of control[true-> positive oder, false->negative oder]
+ * @param controls :output the control commond
+ * @return if exist return true else return false
+ */
+bool CC_ElementaryControls(HC_CC_Circle &c, const Configuration &q, double delta, bool order, vector<Control> &controls);
 
 }
 
